88551fda49d15c720d7d42e9a98cdbe7be3f1b8c
MIRA-KUMAR <mira.kumar.24@gmail.com>
2024-10-07 18:13:19 -0700

    changes to server

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const app = express();
+const users = [];
+
+const bcrypt = require('bcrypt');
 
 app.set('view-engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     res.render('index.ejs', {name: 'Mira'});
 })
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs');
+})
+
+
+app.post('/register', async (req, res) => {
+    try {
+        const hashPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email, 
+            password: hashPassword
+        })
+        res.redirect('/login');
+    } catch (err) {
+        console.error('Register failed!', err);
+        res.redirect('/register');
+    }
+    console.log(users);
+})
+
 app.listen(3000);
